2c95483cde7b23fc4991aa21618de33a6b0355b8
Dean <dean3712@gmail.com>
2020-07-10 14:47:50 -0600

    Base movement made

---
+++ Assets/Scripts/CursorProjector.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorProjector : MonoBehaviour
+{
+    private Camera m_Camera;
+
+    private void Awake()
+    {
+        m_Camera = GetComponent<Camera>();
+    }
+
+    public Vector3 GetScreenProjectPosition(Vector2 mousePosition) 
+    {
+        RaycastHit hit;
+        Ray ray = m_Camera.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out hit, 100f, ~LayerMask.NameToLayer("Ground")))
+        {
+            return hit.point;
+        }
+        else
+        {
+            return Vector3.zero;
+        }
+    }
+}
---
+++ Assets/Scripts/Robot.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Robot : MonoBehaviour
+{
+    public CursorProjector m_CursorProjector;
+    public float m_BaseSpeed = 2.0f;
+
+    private Rigidbody m_Rigidbody;
+
+    private void Awake()
+    {
+        m_Rigidbody = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        Vector3 position = m_CursorProjector.GetScreenProjectPosition(Input.mousePosition);
+        MoveToPoint(position);
+    }
+
+    void MoveToPoint(Vector3 point) 
+    {
+        //Vector3 newPosition = Vector3.Lerp(transform.position, point, Time.deltaTime);
+        //transform.position = newPosition;
+
+        Vector3 direction = point - transform.position;
+        direction = direction.normalized;
+
+        m_Rigidbody.velocity = direction * m_BaseSpeed;
+
+        if (point == Vector3.zero) 
+        {
+            Debug.Log(point);
+        }
+    }
+}
