a89fc4a35f9bf59d601ed0cf2e6e210e824bb1d8
Dean <dean3712@gmail.com>
2020-07-10 17:03:34 -0600

    Implemented speed up mechanic

---
+++ Assets/Scripts/OverloadController.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class OverloadController : MonoBehaviour
+{
+    public float m_SpeedBoostTimeMinTime = 10f;
+    public float m_SpeedBoostTimeMaxTime = 20f;
+    public float m_BoostTime = 3.0f;
+
+    public UnityEvent m_OnSpeedBoostStart;
+    public UnityEvent m_OnSpeedBoostEnd;
+
+    private float m_LastSpeedBoostTime;
+    private float m_SpeedBoostDelay;
+    private bool m_IsBoosting;
+
+    private void Start()
+    {
+        m_SpeedBoostDelay = Random.Range(m_SpeedBoostTimeMinTime, m_SpeedBoostTimeMaxTime);
+    }
+
+    private void Update()
+    {
+        if (Time.time - m_LastSpeedBoostTime > m_SpeedBoostDelay) 
+        {
+            StartSpeedBoost();
+        }
+
+        if (m_IsBoosting) 
+        {
+            if (Time.time - m_LastSpeedBoostTime > m_BoostTime)
+            {
+                EndSpeedBoost();
+            }
+        }
+
+    }
+
+    private void StartSpeedBoost() 
+    {
+        m_IsBoosting = true;
+
+        m_OnSpeedBoostStart?.Invoke();
+
+        m_LastSpeedBoostTime = Time.time;
+        m_SpeedBoostDelay = Random.Range(m_SpeedBoostTimeMinTime, m_SpeedBoostTimeMaxTime);
+    }
+
+    private void EndSpeedBoost() 
+    {
+        m_IsBoosting = false;
+
+        m_OnSpeedBoostEnd?.Invoke();
+    }
+}
--- Assets/Scripts/Robot.cs
+++ Assets/Scripts/Robot.cs
@@ -5,20 +5,39 @@ using UnityEngine;
 public class Robot : MonoBehaviour
 {
     public float m_TotalEnergy = 100f;
-    public float m_BaseSpeed = 2.0f;
-    public bool m_AlternateScheme = false;
 
+    [Header("Speed Controls")]
+    public float m_BaseSpeed = 2.0f;
+    public float m_BaseTurnSpeed = 1.0f;
+    public float m_SpeedBoostSpeed = 30.0f;
+
+    private OverloadController m_OverloadController;
     private Rigidbody m_Rigidbody;
+
+    private float m_Speed;
     private float m_CurrentEnergy;
 
     private void Awake()
     {
         m_Rigidbody = GetComponent<Rigidbody>();
+        m_OverloadController = GetComponent<OverloadController>();
     }
 
     private void Start()
     {
         m_CurrentEnergy = m_TotalEnergy;
+        m_Speed = m_BaseSpeed;
+    }
+
+    private void OnEnable()
+    {
+        m_OverloadController.m_OnSpeedBoostStart.AddListener(OnSpeedBoostStart);
+        m_OverloadController.m_OnSpeedBoostEnd.AddListener(OnSpeedBoostEnd);
+    }
+
+    private void OnDisable()
+    {
+        m_OverloadController.m_OnSpeedBoostStart.RemoveAllListeners();
     }
 
     void Update()
@@ -38,6 +57,18 @@ public class Robot : MonoBehaviour
             transform.Rotate(0, 0.5f, 0);
         }
 
-        m_Rigidbody.velocity = transform.forward * m_BaseSpeed;
+        m_Rigidbody.velocity = transform.forward * m_Speed;
+    }
+
+    void OnSpeedBoostStart() 
+    {
+        Debug.Log("Speeding Up!!!");
+        m_Speed = m_SpeedBoostSpeed;
+    }
+
+    void OnSpeedBoostEnd() 
+    {
+        Debug.Log("It chill now");
+        m_Speed = m_BaseSpeed;
     }
 }
