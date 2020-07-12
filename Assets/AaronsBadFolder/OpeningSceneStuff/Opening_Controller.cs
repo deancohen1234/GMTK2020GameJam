0e3b75a0ef77da2e7aa7e87476a9fd8862867d59
OnettAC <aaroncgamedesign@gmail.com>
2020-07-12 06:27:53 -0600

    Added Opening scene and functionality

---
+++ Assets/AaronsBadFolder/OpeningSceneStuff/Opening_Controller.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Opening_Controller : MonoBehaviour
+{
+    public bool MenuActive;
+    public Animator OpeningAnimator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        MenuActive = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (MenuActive)
+        {
+            if (Input.GetKeyDown(KeyCode.Return))
+            {
+                OpeningAnimator.SetBool("StartGame", true);
+            }
+        }
+    }
+
+    public void StartMenu()
+    {
+        MenuActive = true;
+    }
+
+    public void Transition()
+    {
+        SceneManager.LoadScene("Lawn Mover");
+    }
+}
